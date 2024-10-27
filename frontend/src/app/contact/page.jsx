c82fa6f8af0dfaf4432d1b9f4099941432408de0
Prateekvaish-ai <prateekvaish449@gmail.com>
2024-10-27 13:51:10 +0530

    React library

---
+++ frontend/src/app/contact/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className='text-center my-5 font-bold text-3xl'>Contact</h1>
+      <p style={{ color: 'red', fontWeight: 'bold', fontSize: 50 }}> JSX is the combination of html and js</p>
+    </div>
+  )
+}
+
+export default Contact
---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (Home) => {
+  return (
+    <div>
+      <h1 className='text-center my-5 font-bold text-3xl'>Home Page</h1>
+    </div>
+  )
+}
+
+export default Home;
